ef2245f642208580f1619e0bde714e2728410940
Aldo Santos <aldoacesso@gmail.com>
2023-08-11 13:34:42 -0300

    Tratando exceções

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/** Só JS */
 // document.addEventListener('DOMContentLoaded', function() {
 //     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
 //         const xhttp = new XMLHttpRequest();
@@ -9,6 +10,7 @@
 //     })
 // })
 
+/** Utilizando o jQuery */
 $(document).ready(function() {
     $('#cep').mask('00000-000');
 
@@ -19,6 +21,7 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
+        /** Utilização do Ajax */
         //$.ajax(endpoint).done(function(resposta) {
         //const logradouro = resposta.logradouro;
         //    const bairro = resposta.bairro;
@@ -32,20 +35,28 @@ $(document).ready(function() {
         //        $(botao).find('span').addClass('d-none');
         //    }, 4000);
         //})
-        fetch(endpoint).then(function(resposta) {
+
+        /** Utilizando o Fetch API com tratamento */
+        fetch(endpoint)
+        .then(function(resposta) {
             return resposta.json();
-        }).then(function(json) {
+        })
+        .then(function(json) {
             const logradouro = json.logradouro;
             const bairro = json.bairro;
             const cidade = json.localidade;
             const estado = json.uf;
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco)
-        
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.")
+        })
+        .finally(function() {
             setTimeout(function() {
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
-            }, 4000);
+            }, 1000);
         })
     })
 })
