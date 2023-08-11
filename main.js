617259aa4900d91800e3934c69f40f78abc93f68
Aldo Santos <aldoacesso@gmail.com>
2023-08-11 11:27:18 -0300

    Adicionando máscara no CEP

--- main.js
+++ main.js
@@ -10,6 +10,8 @@
 // })
 
 $(document).ready(function() {
+    $('#cep').mask('00000-000');
+
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
